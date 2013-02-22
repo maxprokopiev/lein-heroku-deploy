d366ef9a112d604a27fbda52a3f5a3830e56f40e
Max Prokopiev <max-prokopiev@yandex.ru>
2013-02-23 01:44:40 +0400

    Basic implementation

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject lein-heroku-deploy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leinengen plugin to simplify Heroku deploy"
+  :url "http://github.com/juggler/lein-heroku-deploy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[clansi "1.0.0"]]
   :eval-in-leiningen true)
--- src/leiningen/heroku_deploy.clj
+++ src/leiningen/heroku_deploy.clj
@@ -1,6 +1,54 @@
-(ns leiningen.heroku-deploy)
+(ns leiningen.heroku-deploy
+  (:use clansi)
+  (:require [clojure.java.shell :refer [sh]]
+            [clojure.string :as string]))
+
+(def ^:dynamic *heroku-app-name* nil)
+
+(defmacro with-app-name
+  "Sets the app name to use with heroku"
+  [app-name & forms]
+  `(binding [*heroku-app-name* ~app-name]
+     ~@forms))
+
+(defn print-command
+  [{exit :exit out :out err :err}]
+  (if (string/blank? out)
+    (println (style (string/trim-newline err) :red))
+    (println (style (string/trim-newline out) :green))))
+
+(defn deploy []
+  (print-command
+    (sh "git" "push" "-f" "heroku" "master")))
+
+(defn heroku-command
+  [command]
+  (print-command
+    (sh "heroku" command "--app" *heroku-app-name*)))
+
+(defn restart []
+  (heroku-command "restart"))
+
+(defn enable-maintenance []
+  (heroku-command "maintenance:on"))
+
+(defn disable-maintenance []
+  (heroku-command "maintenance:off"))
+
+(defn warm-instance
+  [app-url]
+  (Thread/sleep 5000)
+  (println (str "Accessing " app-url " to warm up your application"))
+  (print-command
+    (sh "curl" "-Il" app-url)))
 
 (defn heroku-deploy
-  "I don't do a lot."
+  "Deploys app to heroku"
   [project & args]
-  (println "Hi!"))
+  (with-app-name (:app-name (:heroku project))
+    (println (str "Deploying "*heroku-app-name* "..."))
+    (enable-maintenance)
+    (deploy)
+    (restart)
+    (disable-maintenance)
+    (warm-instance (:app-url (:heroku project)))))
